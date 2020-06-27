b5d40e3284c779a1751ff14d8df8dfd465814d1c
bulsond <bulsond@gmail.com>
2020-06-27 13:11:10 +0300

    Fibonacci вычисление рекурсивно

---
+++ Fibonacci/recurs1.js
@@ -0,0 +1,11 @@
+// Вычисление числа Фибоначчи как суммы двух предыдущих чисел
+// последовательность получается такая: 1, 1, 2, 3, 5, 8
+// Рекурсивно
+
+function fib(number) {
+  if (number < 2) return 1;
+  return fib(number - 1) + fib(number - 2);
+}
+
+const fibNum = fib(4);
+console.log(fibNum);
